7a207e8e349c3adcc885d0c9708f7e501aadab01
Rui Teixeira <rteixeira@beeverycreative.com>
2014-09-18 18:02:04 +0100

    Update worker.sh

--- beetf/firmware/worker.sh
+++ beetf/firmware/worker.sh
@@ -1,3 +1,3 @@
-#!bin/bash
-python /home/pi/beeconnect/beetf/examples/flash_launch.py /home/pi/beeconnect/firmware/beeconnect_v0.1.bin
+#!/bin/sh
+python /home/pi/beeconnect/beetf/examples/flash_launch.py /home/pi/beeconnect/beetf/firmware/beeconnect_v0.1.bin
 exit
