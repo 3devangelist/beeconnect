eb8b8f078ab07b8fd064f3c14d5e5c13bd7970b7
Rui Teixeira <rteixeira@beeverycreative.com>
2014-09-18 17:49:48 +0100

    Update launch_beeconnect.sh

--- beetf/firmware/launch_beeconnect.sh
+++ beetf/firmware/launch_beeconnect.sh
@@ -1,2 +1,2 @@
 #!bin/bash
-python /home/pi/beeconnect/beetf/examples/flash_launch.py /home/pi/beeconnect/beetf/firmware/beeconnect_v0.1.bin & exit
+/home/pi/beeconnect/beetf/firmware/worker.sh & exit
