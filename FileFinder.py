2718397c46e885f40051ff8a7b9558720c03c15e
marcosfg@ua.pt <marcosfg@ua.pt>
2015-10-27 17:56:30 +0000

    Python2 compatibility

--- FileFinder.py
+++ FileFinder.py
@@ -80,7 +80,7 @@ class FileFinder():
         
         """
         
-        path = self.homePath + "/beeconnect" + relPath
+        path = str(self.homePath + "/beeconnect" + relPath)
         
         return path
     
